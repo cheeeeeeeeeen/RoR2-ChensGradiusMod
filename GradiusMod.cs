47f491e63167ea7dbb9d8da2d9cbdf346a1fbe10
cheeeeeeeeeen <blancfaye7@gmail.com>
2020-11-13 02:21:46 +0800

    Update version and turn off debug mode

--- GradiusMod.cs
+++ GradiusMod.cs
@@ -1,4 +1,4 @@
-﻿#define DEBUG
+﻿#undef DEBUG
 
 using BepInEx;
 using BepInEx.Configuration;
@@ -28,7 +28,7 @@ namespace Chen.GradiusMod
 #if DEBUG
             "0." +
 #endif
-            "1.7.0";
+            "2.0.0";
 
         public const string ModName = "ChensGradiusMod";
         public const string ModGuid = "com.Chen.ChensGradiusMod";
