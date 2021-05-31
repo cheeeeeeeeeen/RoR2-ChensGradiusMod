96242c23ce3461aeaab614f7daf47b8cb126f358
cheeeeeeeeeen <blancfaye7@gmail.com>
2021-05-31 22:06:26 +0800

    Update tests

---
+++ ChensGradiusMod.Tests/Items/OptionSeed/OptionSeed.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassBeingTested = Chen.GradiusMod.Items.OptionSeed.OptionSeed;
+
+namespace Chen.GradiusMod.Tests.Items.OptionSeed
+{
+    [TestClass]
+    public class OptionSeed
+    {
+        [TestMethod]
+        public void DebugCheck_Toggled_ReturnsFalse()
+        {
+            bool result = ClassBeingTested.DebugHookCheck();
+
+            Assert.IsFalse(result);
+        }
+    }
+}
