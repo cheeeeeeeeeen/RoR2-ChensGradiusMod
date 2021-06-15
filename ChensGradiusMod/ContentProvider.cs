fac2adfa162ac96d6ea134d87b6a55f92531bfce
cheeeeeeeeeen <blancfaye7@gmail.com>
2021-06-15 21:11:52 +0800

    Integrate Generic Content Pack provider

--- ChensGradiusMod/ContentProvider.cs
+++ ChensGradiusMod/ContentProvider.cs
@@ -1,51 +1,20 @@
 ﻿using RoR2.ContentManagement;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Chen.GradiusMod
 {
-    internal class ContentProvider : IContentPackProvider
+    internal class ContentProvider : GenericContentPackProvider
     {
-        internal ContentPack contentPack = new ContentPack();
-
         internal List<GameObject> bodyObjects = new List<GameObject>();
-
         internal List<GameObject> masterObjects = new List<GameObject>();
 
-        public string identifier => GradiusModPlugin.ModGuid;
+        protected override string ContentIdentifier() => GradiusModPlugin.ModGuid;
 
-        public void Initialize()
+        protected override void LoadStaticContentAsyncActions(LoadStaticContentAsyncArgs args)
         {
-            ContentManager.collectContentPackProviders += ContentManager_collectContentPackProviders;
-        }
-
-        private void ContentManager_collectContentPackProviders(ContentManager.AddContentPackProviderDelegate addContentPackProvider)
-        {
-            addContentPackProvider(this);
-        }
-
-        public IEnumerator LoadStaticContentAsync(LoadStaticContentAsyncArgs args)
-        {
-            contentPack.identifier = identifier;
             contentPack.bodyPrefabs.Add(bodyObjects.ToArray());
             contentPack.masterPrefabs.Add(masterObjects.ToArray());
-
-            args.ReportProgress(1f);
-            yield break;
-        }
-
-        public IEnumerator GenerateContentPackAsync(GetContentPackAsyncArgs args)
-        {
-            ContentPack.Copy(contentPack, args.output);
-            args.ReportProgress(1f);
-            yield break;
-        }
-
-        public IEnumerator FinalizeAsync(FinalizeAsyncArgs args)
-        {
-            args.ReportProgress(1f);
-            yield break;
         }
     }
 }
