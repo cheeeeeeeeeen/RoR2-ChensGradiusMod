aa2a738f33c88f555d872ac11a99182b313a545a
cheeeeeeeeeen <blancfaye7@gmail.com>
2021-06-17 19:26:52 +0800

    Allow equipment drones be have their drivers modifed as well

--- ChensGradiusMod/GradiusMod.Plugin.cs
+++ ChensGradiusMod/GradiusMod.Plugin.cs
@@ -183,6 +183,7 @@ namespace Chen.GradiusMod
             missileDroneMaster.SetAllDriversToAimTowardsEnemies();
             backupDroneMaster.SetAllDriversToAimTowardsEnemies();
             flameDroneMaster.SetAllDriversToAimTowardsEnemies();
+            equipmentDroneMaster.SetAllDriversToAimTowardsEnemies();
         }
 
         internal static bool DebugCheck()
